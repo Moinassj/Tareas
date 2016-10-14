942df8a4092aa3f994e4b440f5c8c0d847eeb91d
Moyses Nev <moyrene84@gmail.com>
2016-10-13 18:59:16 -0700

    Primer tarea

---
+++ Tarea1/Tarea1/ClassEmpleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1
+{
+    public class ClassEmpleado
+    {
+        // variables
+        public string Nombre;
+        public int Edad;
+        public double Salario;
+        public double Total;
+        public double IVA;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ClassEmpleado()
+        {
+            Nombre = "";
+            Edad = 0;
+            Salario = 0.0;
+        }
+    }
+}
---
+++ Tarea1/Tarea1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double total;
+            string linea;
+            ClassEmpleado empleado = new ClassEmpleado();
+            //Ingresa nombre
+            Console.WriteLine("Ingresa nombre: ");
+            empleado.Nombre = Console.ReadLine();
+            //Ingresa edad
+            Console.WriteLine("Ingresa Edad: ");
+            linea = Console.ReadLine();
+            empleado.Edad = int.Parse(linea);
+            //Ingresa salario
+            Console.WriteLine("Ingresa Salario: ");
+            linea = Console.ReadLine();
+            empleado.Salario = double.Parse(linea);
+
+            if (empleado.Salario >= 10000)
+            {
+                empleado.IVA = 0.20 * empleado.Salario;
+                total = empleado.Salario - empleado.IVA;
+                Console.WriteLine("tu salario es de : ");
+                Console.WriteLine(total);
+            }
+            else
+            {
+                empleado.IVA = 0.16 * empleado.Salario;
+                total = empleado.Salario - empleado.IVA;
+                Console.WriteLine("tu salario es de : {1}", total);
+                Console.WriteLine(total);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
