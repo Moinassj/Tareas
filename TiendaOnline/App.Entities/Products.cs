07ca54507d39ea26c82715c2f9f430c9906c565c
Moyses Nev <moyrene84@gmail.com>
2016-11-21 18:19:03 -0800

    tarea Tienda online

---
+++ TiendaOnline/App.Entities/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price{ get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public DateTime UpDate { get; set; }
+    }
+}
